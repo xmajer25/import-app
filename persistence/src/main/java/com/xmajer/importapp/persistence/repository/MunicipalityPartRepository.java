d7d9d781eb5e777cb1cb2bcf04bf07d7cbee7703
Jakub Majer <567782@mail.muni.cz>
2026-09-23 13:08:59 +0200

    fix: repo ids to String and spring setup

--- persistence/src/main/java/com/xmajer/importapp/persistence/repository/MunicipalityPartRepository.java
+++ persistence/src/main/java/com/xmajer/importapp/persistence/repository/MunicipalityPartRepository.java
@@ -3,7 +3,5 @@ package com.xmajer.importapp.persistence.repository;
 import com.xmajer.importapp.persistence.entity.MunicipalityPart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface MunicipalityPartRepository extends JpaRepository<MunicipalityPart, UUID> {
+public interface MunicipalityPartRepository extends JpaRepository<MunicipalityPart, String> {
 }
--- persistence/src/main/java/com/xmajer/importapp/persistence/repository/MunicipalityRepository.java
+++ persistence/src/main/java/com/xmajer/importapp/persistence/repository/MunicipalityRepository.java
@@ -3,7 +3,5 @@ package com.xmajer.importapp.persistence.repository;
 import com.xmajer.importapp.persistence.entity.Municipality;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface MunicipalityRepository extends JpaRepository<Municipality, UUID> {
+public interface MunicipalityRepository extends JpaRepository<Municipality, String> {
 }
