cab236c6f8fa1745ec3b94587354425681210573
Jakub Majer <567782@mail.muni.cz>
2026-09-23 13:10:06 +0200

    feat: prepare environment

--- api/src/main/java/com/xmajer/importapp/api/ApiApplication.java
+++ api/src/main/java/com/xmajer/importapp/api/ApiApplication.java
@@ -1,9 +1,12 @@
 package com.xmajer.importapp.api;
 
+import com.xmajer.importapp.persistence.config.PersistenceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(PersistenceConfig.class)
 public class ApiApplication {
 
     public static void main(String[] args) {
--- importer/src/main/java/com/xmajer/importapp/importer/ImporterApplication.java
+++ importer/src/main/java/com/xmajer/importapp/importer/ImporterApplication.java
@@ -1,9 +1,12 @@
 package com.xmajer.importapp.importer;
 
+import com.xmajer.importapp.persistence.config.PersistenceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(PersistenceConfig.class)
 public class ImporterApplication {
 
     public static void main(String[] args) {
