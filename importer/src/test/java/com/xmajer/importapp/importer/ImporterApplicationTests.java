fafe010d73d82951925ebefc24ca37edf255e9dd
Jakub Majer <567782@mail.muni.cz>
2026-09-23 10:43:28 +0200

    Initial Commit with Structure

---
+++ importer/src/test/java/com/xmajer/importapp/importer/ImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmajer.importapp.importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
